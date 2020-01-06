ee2d665a475bcefddf6f04e42fae44caa621fc57
FaramarzAf <faramarzafzali@gmail.com>
2020-01-06 23:53:52 +0330

    screen shots added

--- app/src/main/java/com/faramarz/tictacdev/mycustomcounter/MainActivity.java
+++ app/src/main/java/com/faramarz/tictacdev/mycustomcounter/MainActivity.java
@@ -16,17 +16,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        valueSelector = new ValueSelector(this);
         valueSelector = findViewById(R.id.value_selector);
-        get = findViewById(R.id.get);
-        get.setOnClickListener(this);
-        //valueSelector.setCustomFont(this, "faryekan.ttf");
-
+       /* get = findViewById(R.id.get);
+        get.setOnClickListener(this);*/
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "" + valueSelector.getValue(), Toast.LENGTH_SHORT).show();
+        String value = String.valueOf(valueSelector.getValue());
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
     }
 
 }
