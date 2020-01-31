989c6e51d7c33000f7b02aacbbac2fff6d3ea6ae
FaramarzAf <faramarzafzali@gmail.com>
2020-02-01 00:27:02 +0330

    clean code

--- app/src/main/java/com/faramarz/tictacdev/mycustomcounter/MainActivity.java
+++ app/src/main/java/com/faramarz/tictacdev/mycustomcounter/MainActivity.java
@@ -1,31 +1,18 @@
 package com.faramarz.tictacdev.mycustomcounter;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ValueSelector valueSelector;
-    Button get;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        valueSelector = findViewById(R.id.value_selector);
 
-       /* get = findViewById(R.id.get);
-        get.setOnClickListener(this);*/
     }
 
-    @Override
-    public void onClick(View v) {
-        String value = String.valueOf(valueSelector.getValue());
-        Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-    }
 
 }
